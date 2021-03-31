bf4c2fd5068302db6ae47bb906415550fae1d919
muradmemmedli <murad.mammadli@yahoo.com>
2021-03-31 10:42:06 +0400

    *

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.style.AlignmentSpan;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
@@ -59,21 +61,59 @@ public class MainActivity extends AppCompatActivity {
         HashMap<Integer, List<Credit>> creditMap = new HashMap<>();
         HashMap<Integer, List<Contact>> contactMap = new HashMap<>();
         HashMap<Integer, List<Account>> accountMap = new HashMap<>();
-        HashMap<Integer, List<Job>> jobs = new HashMap<>();
+        HashMap<Integer, List<Job>> jobMap = new HashMap<>();
+
         for (Debt debt : debtList) {
-            debtMap.put(debt.getUserId(), debtList);
+            if (debtMap.containsKey(debt.getUserId())) {
+                List<Debt> list = debtMap.get(debt.getUserId());
+                list.add(debt);
+            } else {
+                List<Debt> list = new ArrayList<>();
+                list.add(debt);
+                debtMap.put(debt.getUserId(), list);
+            }
         }
+
+
         for (Credit credit : creditList) {
-            creditMap.put(credit.getUserId(), creditList);
+            if (creditMap.containsKey(credit.getUserId())) {
+                List<Credit> list = creditMap.get(credit.getUserId());
+                list.add(credit);
+            } else {
+                List<Credit> list = new ArrayList<>();
+                list.add(credit);
+                creditMap.put(credit.getUserId(), list);
+            }
         }
         for (Contact contact : contactList) {
-            contactMap.put(contact.getUserId(), contactList);
+            if (contactMap.containsKey(contact.getUserId())) {
+                List<Contact> list = contactMap.get(contact.getUserId());
+                list.add(contact);
+            } else {
+                List<Contact> list = new ArrayList<>();
+                list.add(contact);
+                contactMap.put(contact.getUserId(), list);
+            }
         }
         for (Account account : accountList) {
-            accountMap.put(account.getUserId(), accountList);
+            if (accountMap.containsKey(account.getUserId())) {
+                List<Account> list = accountMap.get(account.getUserId());
+                list.add(account);
+            } else {
+                List<Account> list = new ArrayList<>();
+                list.add(account);
+                accountMap.put(account.getUserId(), list);
+            }
         }
         for (Job job : jobList) {
-            jobs.put(job.getUserId(), jobList);
+            if (jobMap.containsKey(job.getUserId())) {
+                List<Job> list = jobMap.get(job.getUserId());
+                list.add(job);
+            } else {
+                List<Job> list = new ArrayList<>();
+                list.add(job);
+                jobMap.put(job.getUserId(), list);
+            }
         }
 
 
@@ -82,21 +122,13 @@ public class MainActivity extends AppCompatActivity {
             user.setAccountList(accountMap.get(user.getId()));
             user.setContactList(contactMap.get(user.getId()));
             user.setCreditList(creditMap.get(user.getId()));
-            user.setJobList(jobs.get(user.getId()));
+            user.setJobList(jobMap.get(user.getId()));
         }
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
-        mAdapter = new myAdapter(this,userList);
+        mAdapter = new myAdapter(this, userList);
         recyclerView.setAdapter(mAdapter);
-
-
-//        for (User user : userList) {
-//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-//                Debt debt = debtList.stream().filter(a -> a.getId() == user.getId()).collect(Collectors.toList()).get(0);
-//                Log.d("debt", String.valueOf(debt));
-//            }
-//        }
 
     }
 }
